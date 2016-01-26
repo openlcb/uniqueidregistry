259dd3af75b154f23f6b463aefbe6deb322d2b7c
Paul Beard <paulbeard@paulbeard.name>
2016-01-26 08:16:57 +0000

    JSON pretty print without PHP 5.4.
    git-svn-id: svn://svn.code.sf.net/p/openlcb-demo/code/trunk/web@18 8a23e261-315c-4324-93ae-7aba059052bf

--- uidjson.php
+++ uidjson.php
@@ -7,8 +7,8 @@ mysql_connect($opts['hn'],$opts['un'],$opts['pw']);
 @mysql_select_db($opts['db']) or die( "Unable to select database. Error (" . mysql_errno() . ") " . mysql_error());
 
 function value($result, $j, $index) {
-    if (255 == mysql_result($result, $j, "uniqueid_byte" . $index . "_mask")) return "*";
-    else return mysql_result($result, $j, "uniqueid_byte" . $index . "_value");
+  if (255 == mysql_result($result, $j, "uniqueid_byte" . $index . "_mask")) return "*";
+  else return mysql_result($result, $j, "uniqueid_byte" . $index . "_value");
 }
 
 $query = "SELECT *
@@ -24,27 +24,32 @@ ORDER BY
   uniqueid_byte5_value;";
 $result = mysql_query($query);
 
-$json = array();
-
-for ($j = 0; $j < mysql_numrows($result); $j++) {
-  $entry = array();
-  $entry['byte0'] = value($result, $j, "0");
-  $entry['byte1'] = value($result, $j, "1");
-  $entry['byte2'] = value($result, $j, "2");
-  $entry['byte3'] = value($result, $j, "3");
-  $entry['byte4'] = value($result, $j, "4");
-  $entry['byte5'] = value($result, $j, "5");
-  if (mysql_result($result, $j, "person_organization") != '') {
-    $entry['organization'] = mysql_result($result, $j, "person_organization");
-  } else {
-    $entry['firstname'] = mysql_result($result, $j, "person_first_name");
-    $entry['lastname'] = mysql_result($result, $j, "person_last_name");
-  }
-  $entry['url'] = mysql_result($result, $j, "uniqueid_url");
-  $entry['comment'] = mysql_result($result, $j, "uniqueid_user_comment");
-  $json[] = $entry;
-}
-
 header('Content-Type: application/json');
 
-echo json_encode($json);
+echo "[\n";
+
+for ($j = 0; $j < mysql_numrows($result); $j++) {
+  echo "  {\n";
+  echo '    "byte0": ' . json_encode(value($result, $j, "0")) . ",\n";
+  echo '    "byte1": ' . json_encode(value($result, $j, "1")) . ",\n";
+  echo '    "byte2": ' . json_encode(value($result, $j, "2")) . ",\n";
+  echo '    "byte3": ' . json_encode(value($result, $j, "3")) . ",\n";
+  echo '    "byte4": ' . json_encode(value($result, $j, "4")) . ",\n";
+  echo '    "byte5": ' . json_encode(value($result, $j, "5")) . ",\n";
+  if (mysql_result($result, $j, "person_organization") != '') {
+    echo '    "organization": ' . json_encode(mysql_result($result, $j, "person_organization")) . ",\n";
+  } else {
+    echo '    "firstname": ' . json_encode(mysql_result($result, $j, "person_first_name")) . ",\n";
+    echo '    "lastname": ' . json_encode(mysql_result($result, $j, "person_last_name")) . ",\n";
+  }
+  echo '    "url": ' . json_encode(mysql_result($result, $j, "uniqueid_url")) . ",\n";
+  echo '    "comment": ' . json_encode(mysql_result($result, $j, "uniqueid_user_comment")) . "\n";
+  if ($j + 1 < mysql_numrows($result)) {
+    echo "  },\n";
+  } else {
+    echo "  }\n";
+  }
+}
+
+
+echo "]";
