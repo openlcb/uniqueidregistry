cdf6d9e2c0c9d0a0e8e710007c38d2f347790772
Alex Shepherd <kiwi64ajs@gmail.com>
2016-02-12 00:40:21 +1300

    now the SQL sets new email address for "The OpenLCB Group" and for Alex Shepherd now deletes a bunch of records in the person table that don't have a uniqueid range allocated as they are probably just hackers/spammers except for the few exceptions I (AJS) eye-balled

--- patch.sql
+++ patch.sql
@@ -8,7 +8,18 @@ ALTER TABLE Person ADD COLUMN person_password_hash VARCHAR(255) NULL;
 
 -- person_is_moderator
 ALTER TABLE Person ADD COLUMN person_is_moderator VARCHAR(1) NOT NULL DEFAULT 'n';
-UPDATE Person SET person_is_moderator = 'y' WHERE person_id IN (3, 4);
+UPDATE Person SET person_is_moderator = 'y' WHERE person_id IN (3, 4, 24);
+
+-- remove all the records from person that don't have a uniqueid range allocated as they 
+-- are probably just hackers/spammers except for the few exceptions I (AJS) eye-balled 
+
+DELETE FROM Person WHERE
+NOT EXISTS (SELECT 0 FROM UniqueIDs WHERE UniqueIDs.person_id = Person.person_id) -- person has no uniqueid
+AND Person.person_id NOT IN ( 18, 138, 139, 145, 147, 149 );
+
+-- change the main email address for all The OpenLCB Group ranges 
+UPDATE Person SET person_email = 'registry@openlcb.org' WHERE person_id = 1;
+UPDATE Person SET person_email = 'kiwi64ajs@gmail.com' WHERE person_id = 3;
 
 -- uniqueid_created
 ALTER TABLE UniqueIDs ADD COLUMN uniqueid_created TIMESTAMP NULL;
